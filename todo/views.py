3a744f3647598b0abe2bfcccce5f864a96353d30
Oleh M <oleh.murachov28@gmail.com>
2026-05-31 10:38:18 +0200

    add tagsand home

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Task, Tag
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.prefetch_related("tags").all().order_by(
+        "is_done", "-created_at"
+    )
+    return render(request, "todo/task_list.html", {"tasks": tasks})
+
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, "todo/tag_list.html", {"tags": tags})

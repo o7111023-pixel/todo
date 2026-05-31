ac806370e2e039755fb9e5e9ab806fbdbafb6682
Oleh M <oleh.murachov28@gmail.com>
2026-05-31 10:50:19 +0200

    Todo app complete

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from . import views
+from .views import TaskCreateView
 
 app_name = "todo"
 
 urlpatterns = [
     path("", views.task_list, name="task-list"),
     path("tags/", views.tag_list, name="tag-list"),
+
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/toggle/", views.task_toggle, name="task-toggle"),
+
+    path("tags/create/", views.TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", views.TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", views.TagDeleteView.as_view(), name="tag-delete"),
 ]
